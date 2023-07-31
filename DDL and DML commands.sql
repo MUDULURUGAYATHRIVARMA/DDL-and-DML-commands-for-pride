bedbadc366942600c48c0a435180e8fdff2a43d3
MUDULURUGAYATHRIVARMA <141038133+MUDULURUGAYATHRIVARMA@users.noreply.github.com>
2023-07-31 20:08:37 +0530

    Update DDL and DML commands.sql

--- DDL and DML commands.sql
+++ DDL and DML commands.sql
@@ -1,30 +1,30 @@
-use jaswanth;
+use Gayathri;
 create table names(
 personid int,
-lastname varchar(234),
-firstname varchar(123),
-address varchar(123),
-city varchar(123)
+lastname varchar(457),
+firstname varchar(803),
+address varchar(703),
+area varchar(563)
 );
 
-create table varma as select lastname,firstname from names;
+create table gayu as select lastname,firstname from names;
 
-select * from varma;
+select * from gayu;
 
-alter table varma add places varchar(2345);
+alter table gayu add places varchar(2345);
 
-select * from varma;
+select * from gayu;
 
-alter table varma drop places;
+alter table gayu drop places;
 set sql_safe_updates=0;
 
 
-alter table varma modify column lastname varchar(150);
-alter table varma add column mobile varchar(234),add column strength int(200),add column power int(200);
-select * from varma;
-insert into varma(lastname,firstname,phone,strength,power) values ('yu','jaswanth','ab',23,45),('tr','sai','vh',23,34),
-('wee','bharath','nh',21,32),('ytr','varma','ag',43,32);
+alter table gayu modify column lastname varchar(150);
+alter table gayu add column mobile varchar(234),add column strength int(200),add column power int(200);
+select * from gayu;
+insert into gayu(lastname,firstname,phone,strength,power) values ('yu','gayathri','ab',23,45),('tr','M','vh',23,34),
+('wee','raju','nh',21,32),('ytr','ram','ag',43,32);
 
-update varma set lastname = 'MARVEL', power=45 WHERE strength=43 ; 
+update gayu set lastname = 'mary', power=45 WHERE strength=43 ; 
 set sql_safe_updates=0;
-update varma set lastname = 'MARVEL', power=45 WHERE strength=43 ; 
+update gayi set lastname = 'mary', power=45 WHERE strength=43 ; 
